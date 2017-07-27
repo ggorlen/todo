33054420ee5eae41a56a1aa40d109f55166970d0
ggorlen <gsgorlen@gmail.com>
2017-07-27 11:43:55 -0700

    small style adjustment

--- show_tasks.php
+++ show_tasks.php
@@ -11,7 +11,7 @@ if ($tasks->num_rows) {
           <td class="centered">' . ($task->due ? substr($task->due, 0, -3) : '-') . 
           '<td class="centered">' . substr($task->created, 0, -3)  . '</td>
           </td><td class="centered"><a id="' . $task->id .
-          '" onclick="deleteTask(this)" href="#"><em>x</em></a></td></tr>';
+          '" onclick="deleteTask(this)" href="javascript:void(0)"><em>x</em></a></td></tr>';
   }
   echo '</table>';
 }
